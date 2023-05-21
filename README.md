73151b6f0909ebffc4658926f42f1b60b0850ad1
SyedRifat <sayedrifat112@gmail.com>
2023-05-21 20:12:46 +0600

    add readme file

---
+++ README.md
@@ -0,0 +1,10 @@
+### Hi there 👋, Syed Rifat
+#### Web Developer
+![Web Developer](https://scontent.fdac24-4.fna.fbcdn.net/v/t39.30808-6/315036039_658701239155418_4599935356432845298_n.jpg?stp=dst-jpg_p640x640&_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_ohc=GLR3Xb5khLMAX_r1ghy&_nc_ht=scontent.fdac24-4.fna&oh=00_AfB-sQz9Q9QOIqQIWkVNjxsDox-uRRvibs0ynnK8HCijdQ&oe=643B40EF)
+
+[Live-link](https://chef-rcipe-hunter.web.app/)
+* In this assignment i create one json file in express by using mongodb.
+* authintication system succesfully done by using firebase .  
+* Fetch data by using useEffect and loader .
+* create post delete update and create method.
+* Create context to  pass data in login and other component .
--- src/Pages/Home/Blogs/Blogs.jsx
+++ src/Pages/Home/Blogs/Blogs.jsx
@@ -8,25 +8,25 @@ const Blogs = () => {
           Q :What is an access token and refresh token? How do they work and
           where should we store them on the client-side?
         </h1>
-        <p className="font-semibold">Ans:</p>
+        <p className="font-semibold">Ans:As mentioned, for security purposes, access tokens may be valid for a short amount of time. Once they expire, client applications can use a refresh token to "refresh" the access token. That is, a refresh token is a credential artifact that lets a client application get new access tokens without having to ask the user to log in again.</p>
       </div>
       <div>
         <h1 className="font-bold text-xl">
           Q:Compare SQL and NoSQL databases?
         </h1>
-        <p className="font-semibold">Ans:</p>
+        <p className="font-semibold">Ans:SQL databases are vertically scalable, while NoSQL databases are horizontally scalable. SQL databases are table-based, while NoSQL databases are document, key-value, graph, or wide-column stores. </p>
       </div>
       <div>
         <h1 className="font-bold text-xl">
           Q:What is express js? What is Nest JS ?
         </h1>
-        <p className="font-semibold">Ans:</p>
+        <p className="font-semibold">Ans:Express is a minimalist and flexible framework that is easy to use and has a large community of developers. NestJS, on the other hand, is a newer framework that provides additional features such as dependency injection</p>
       </div>
       <div>
         <h1 className="font-bold text-xl">
           Q:What is MongoDB aggregate and how does it work?
         </h1>
-        <p className="font-semibold">Ans:</p>
+        <p className="font-semibold">Ans: Aggregation is a way of processing a large number of documents in a collection by means of passing them through different stages. </p>
       </div>
     </div>
   );
